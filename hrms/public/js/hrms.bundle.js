1587c7180a576bb7910ec43a8734f8f554f451ef
ahmad406 <ahmadsayyed66@gmail.com>
2024-03-14 11:14:47 +0530

    location base checkin

---
+++ hrms/public/js/hrms.bundle.js
@@ -0,0 +1,6 @@
+import "./templates/employees_with_unmarked_attendance.html";
+import "./templates/feedback_summary.html";
+import "./templates/feedback_history.html";
+import "./templates/rating.html";
+import "./utils";
+import "./payroll_common";
